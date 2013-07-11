b1798994d186c449dbf270db4f3982e768edfb50
Fiona Klute <fiona2.klute@uni-dortmund.de>
2013-07-11 15:13:50 +0200

    Two more simple generators: Alternate Interval and Random Size

--- src/static_generator.c
+++ src/static_generator.c
@@ -12,6 +12,9 @@ int static_generator_init(generator_t *this);
 int static_generator_destroy(generator_t *this);
 
 int alternate_time_generator_init(generator_t *this);
+int rand_size_generator_init(generator_t *this);
+int rand_size_generator_generate_block(generator_t *this,
+				       struct packet_block *current);
 
 struct static_generator_attr
 {
@@ -63,6 +66,88 @@ int static_generator_init(generator_t *this)
 	{
 		block->data[i].size = attr->size;
 		memcpy(&(block->data[i].delay), &(attr->interval),
+		       sizeof(struct timespec));
+	}
+
+	return 0;
+}
+
+
+
+/* This generator provides two static blocks with the configured
+ * packet size, but one will have doubled intervals. */
+int alternate_time_generator_init(generator_t *this)
+{
+	struct static_generator_attr *attr =
+		(struct static_generator_attr *) this->attr;
+
+	*(this->block) = create_block_circle(2, BLOCK_LEN);
+
+	struct timespec alt_interval;
+	alt_interval.tv_sec = attr->interval.tv_sec;
+	alt_interval.tv_nsec = attr->interval.tv_nsec * 2;
+	int normal = 1;
+
+	struct packet_block *block = *(this->block);
+	do
+	{
+		for (int i = 0; i < block->length; i++)
+		{
+			block->data[i].size = attr->size;
+			if (normal)
+				memcpy(&(block->data[i].delay),
+				       &(attr->interval),
+				       sizeof(struct timespec));
+			else
+				memcpy(&(block->data[i].delay),
+				       &(alt_interval),
+				       sizeof(struct timespec));
+		}
+		block = block->next;
+		normal = !normal;
+	} while (block != *(this->block));
+
+	return 0;
+}
+
+
+
+/* This generator uses a random packet size that is the same
+ * throughout one block. attr->size is used as a maximum. */
+int rand_size_generator_init(generator_t *this)
+{
+	struct static_generator_attr *attr =
+		(struct static_generator_attr *) this->attr;
+
+	*(this->block) = create_block_circle(4, BLOCK_LEN);
+
+	struct packet_block *block = *(this->block);
+	do
+	{
+		rand_size_generator_generate_block(this, block);
+		block = block->next;
+	} while (block != *(this->block));
+
+	return 0;
+}
+
+
+
+/* Refill the block using a new random number for packet
+ * size. Intervals are set to attr->interval. */
+int rand_size_generator_generate_block(generator_t *this,
+				       struct packet_block *current)
+{
+	struct static_generator_attr *attr =
+		(struct static_generator_attr *) this->attr;
+
+	long int r = random() * attr->size / RAND_MAX;
+	if (r < MIN_PACKET_SIZE)
+		r = MIN_PACKET_SIZE;
+	for (int i = 0; i < current->length; i++)
+	{
+		current->data[i].size = r;
+		memcpy(&(current->data[i].delay), &(attr->interval),
 		       sizeof(struct timespec));
 	}
 
