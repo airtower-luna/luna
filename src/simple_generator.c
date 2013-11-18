0924ade22cbd0cab2b87ee5206a9e519fee7f15f
Fiona Klute <fiona2.klute@uni-dortmund.de>
2013-11-18 13:53:38 +0100

    Random size generator: Get a new random number for each packet
    This should lead to more randomness than using the same size for a whole
    buffer block, at a small performance penalty.

--- src/simple_generator.c
+++ src/simple_generator.c
@@ -214,12 +214,12 @@ int rand_size_generator_fill_block(generator_t *this,
 	struct static_generator_attr *attr =
 		(struct static_generator_attr *) this->attr;
 
-	/* scale random number to range of MIN_PACKET_SIZE to attr->size */
-	long int r = random() * (attr->size - MIN_PACKET_SIZE) / RAND_MAX
-		+ MIN_PACKET_SIZE;
-
 	for (int i = 0; i < current->length; i++)
 	{
+		/* scale random number to range of MIN_PACKET_SIZE to
+		 * attr->size */
+		long int r = random() * (attr->size - MIN_PACKET_SIZE)
+			/ RAND_MAX + MIN_PACKET_SIZE;
 		current->data[i].size = r;
 		memcpy(&(current->data[i].delay), &(attr->interval),
 		       sizeof(struct timespec));
